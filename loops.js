1bc5c8376ea857dffc97767266d7d86c5cae272b
Nate Dukatz <natedukatz@gmail.com>
2018-07-09 04:56:31 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -8,6 +8,17 @@ function forLoop(array) {
 
 function whileLoop(n) {
   while (n > 0) {
-    console.log(n)
+    console.log(n--)
   }
+  return "done"
+}
+
+function doWhileLoop(array) {
+  function maybeTrue() {
+    return Math.random() >= 0.5 // Returns a random number between 0 (inclusive) and 1 (exclusive)
+  }
+
+  do {
+    array.pop()
+  } while(array.length > 0 && maybeTrue())
 }
